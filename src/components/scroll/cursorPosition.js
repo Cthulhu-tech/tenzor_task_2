f33451eaf5b508a61ebf35986c8c6d9d703c9f3c
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-12-15 06:37:35 +0400

    add gulp-ts

--- src/components/scroll/cursorPosition.js
+++
@@ -1,9 +0,0 @@
-const cursor = document.querySelector('.cursor')
-
-const positionCursorHandler = (event) => {
-    cursor.style.display = "block"
-    cursor.style.top = event.clientY + 'px'
-    cursor.style.left = event.clientX + 'px'
-}
-
-window.addEventListener('mousemove', positionCursorHandler)
